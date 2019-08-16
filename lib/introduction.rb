69ac1ec82e26fd7e931381f002522126769c75c4
Joe David <javid92@gmail.com>
2019-08-16 09:09:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ def introduction(name = "Joe")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning  to program in #{language}."
   
-def introduction_with_language(name, language)
+def introduction_with_language(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning  to program in #{language}."
     
   
